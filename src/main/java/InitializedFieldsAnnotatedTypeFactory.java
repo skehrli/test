91872717ae19b7ad2a2e99dbf27de42526f74ea7
Sascha Kehrli <skehrli@ethz.ch>
2025-04-12 15:48:20 +0200

    minimize genericannotatedtypefactory

---
+++ src/main/java/InitializedFieldsAnnotatedTypeFactory.java
@@ -0,0 +1,21 @@
+import org.checkerframework.framework.source.SourceChecker;
+import org.checkerframework.framework.type.AnnotatedTypeFactory;
+import org.checkerframework.framework.type.GenericAnnotatedTypeFactory;
+
+class InitializedFieldsAnnotatedTypeFactory {
+
+  private void defaultValueIsOK(Class<? extends SourceChecker> sourceCheckerClass,
+     GenericAnnotatedTypeFactory<?,?,?,?> gatf,
+     SourceChecker subchecker
+   ) {
+
+    // nothing bad happens when a normal sourceCheckerClass is passed.
+    // this throws no errors.
+    gatf.getTypeFactoryOfSubchecker(sourceCheckerClass);
+
+    // however, when passing subchecker.getClass(), whose return type is precisely
+    // Class<? extends SourceChecker>, the upper bound for the wildcard is TOP instead of bottom,
+    // which results in an error in my type system
+    gatf.getTypeFactoryOfSubchecker(subchecker.getClass());
+  }
+}
